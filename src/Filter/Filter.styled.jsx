cf1d41a7eac0d2408054129edee3ff4497cbf084
go-to-stars <ivanstep@ukr.net>
2023-07-07 00:45:34 +0300

    Update Filter.styled.jsx

---
+++ src/Filter/Filter.styled.jsx
@@ -0,0 +1,10 @@
+import styled from "@emotion/styled";
+
+export const Label = styled.label`
+  display: flex;
+  flex-direction: column;
+`;
+
+export const Input = styled.input`
+  width: 200px;
+`;
